9d915bbb45c6c4a16c062b45fcf143330b1a78a3
ankitjiii <59410552+ankitjiii@users.noreply.github.com>
2020-01-08 20:51:28 +0530

    Add files via upload

---
+++ fibbonaci no..py
@@ -0,0 +1,13 @@
+#this program is completly created by me without any help
+def fibbonaci(n):
+    """fibbonaci no. 0 1 1 2 3 5 8 13 21......"""
+    if n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        return fibbonaci(n-1) + fibbonaci(n-2)
+number= int(input("enter the no."))
+print(fibbonaci(number))
+
+
